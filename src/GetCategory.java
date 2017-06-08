b7d4ee68689ad8ef4910e708fb19f8e63843c4bd
Manjeet Kumar <manjeet.fly@gmail.com>
2017-06-08 09:48:54 +0530

    category changes

--- src/GetCategory.java
+++ src/GetCategory.java
@@ -1,9 +1,15 @@
 
-import opennlp.tools.stemmer.PorterStemmer;
+
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import scala.util.parsing.combinator.testing.Str;
 
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,11 +18,36 @@ import java.util.List;
 /**
  * Created by manjeet on 6/6/17.
  */
-public class GetCategory {
+public class GetCategory{
+
+    private static String getUrlSource(String url) throws IOException {
+        URL yahoo = new URL(url);
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            a.append(inputLine);
+        in.close();
+
+        return a.toString();
+    }
 
     public static ArrayList<Category> getCategories(String url) throws Exception{
         //System.out.println("Start:: " + url);
-        org.jsoup.nodes.Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+        org.jsoup.nodes.Document doc = new Document("");
+
+        if(url.contains("%"))
+        {
+            String temp_url = URLDecoder.decode(url,"UTF-8");
+            //System.out.println("decoded::"+temp_url);
+            doc = Jsoup.parse(getUrlSource(temp_url));
+        }
+        else
+        {
+            doc = Jsoup.connect(url).userAgent("Mozilla/5.0").get();
+        }
+
         //System.out.println(doc.html());
         Elements results = doc.select("div.mw-normal-catlinks > ul");
         ArrayList<Category> catList = new ArrayList<Category>();
@@ -110,29 +141,41 @@ public class GetCategory {
         return allCats;
     }
 
-    public int catgoryScore (String head_phrase, String answer) throws Exception{
+    public ArrayList<String> catgoryScore (String head_phrase, String answer) throws Exception{
         String head = head_phrase;
         String head_s = head + "s";
         String head_es = head + "es";
-        int sum = 0;
+        String head_ies = head.substring(0,head.length() - 1) + "ies";
+        ArrayList<String> res = new ArrayList<String>();
+        double sum = 0;
 //        PorterStemmer p = new PorterStemmer();
 //        String stemmed = p.stem(head);
 //        System.out.println("stem: " +
-        String[] head_split = head.split(" ");
+        String[] head_split = answer.split(" ");
         ArrayList<Category> c1 = getCats(answer);
-//        ArrayList<Category> c2 = getCats("Debuggers");
+        ArrayList<Category> c2 = getCats(head_phrase);
         for (Category c : c1){
-            if(c.getName().matches(head_s) || c.getName().matches((head_es)) || c.getName().matches(head)){
+            if(c.getName().matches(head_s) || c.getName().matches((head_es)) || c.getName().matches(head) || c.getName().matches(head_ies) ){
                 System.out.println("Found plural:: " + c.getName());
                 if(c.getLevel() == 0){
                     System.out.println("plural at level 0");
                     if(c.getName().contains(head)){
                         System.out.println("Found typeof");
+                        res.add("plural" + ";" + answer + ";" + head + ";" + c.getName() + ";" + c.getLevel());
                         sum++;
+                    }
+                }
+                else{
+                    System.out.println("plural at level " + c.getLevel());
+                    if(c.getName().contains(head)){
+                        System.out.println("Found typeof");
+                        res.add("plural" + ";" + answer + ";" + head + ";" + c.getName() + ";" + c.getLevel());
+                        sum = sum + (1.0/c.getLevel());
                     }
                 }
 
             }
+
             List<String> c_split = Arrays.asList(c.getName().split(" "));
 
             if( c_split.size() > head_split.length ){
@@ -154,14 +197,115 @@ public class GetCategory {
                     if(c.getName().contains(head)){
                         System.out.println("Found typeof");
                         sum = sum + 1;
+                        res.add("suffix/prefix" + ";" + answer + ";" + head + ";" + c.getName() + ";" + c.getLevel());
+                    }
+                    else{
+                        res.add("suffix/prefix/W" + ";" + answer + ";" + head + ";" + c.getName() + ";" + c.getLevel());
+                    }
+                }
+            }
+            for(Category c2_temp : c2){
+                if(c2_temp.getName() == c.getName()){
+                    //if(c2_temp.getLevel() < c.getLevel()){
+                    res.add("Category Match" + ";" + answer + ";" + head + ";" + c.getName() + ";" + c.getLevel() + ";" + c2_temp.getName() + ";" + c2_temp.getLevel());
+                    //}
+                }
+            }
+        }
+
+        for(Category c2_temp : c2){
+            List<String> c_split = Arrays.asList(c2_temp.getName().split(" "));
+            if( c_split.size() > head_split.length ){
+                int count = 0;
+                for(int i = 0 ; i < head_split.length ; i++){
+//                    if(!c_split.contains(head_split[i])){
+//                        flag = 1;
+//                        break;
+//                    }
+                    for(String s : c_split){
+                        if(s.contains(head_split[i])){
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if(count == head_split.length){
+                    System.out.println("Found suffix_2 :: " + c2_temp.getName());
+                    if(c2_temp.getName().contains(head)){
+                        System.out.println("Found typeof_2");
+                        res.add("suffix/prefix" + ";" + answer + ";" + head + ";" + c2_temp.getName() + ";" + c2_temp.getLevel());
+                        sum = sum + 1;
+                    }
+                    else{
+                        res.add("suffix/prefix/W" + ";" + answer + ";" + head + ";" + c2_temp.getName() + ";" + c2_temp.getLevel());
                     }
                 }
             }
         }
 
-        return sum;
+        return res;
 
     }
+
+    public static void main(String args[]) throws IOException {
+
+        FileWriter fw = new FileWriter("category_validated.csv");
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        FileReader fr = new FileReader("catInput.csv");
+        BufferedReader br = new BufferedReader(fr);
+        ArrayList<String> head_ent_pair = new ArrayList<String>();
+        String line_url = "";
+        while((line_url = br.readLine()) != null)
+        {
+            head_ent_pair.add(line_url);
+        }
+
+        try{
+
+            if(br != null)
+                br.close();
+            if(fr != null)
+                fr.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        GetCategory getCategory = new GetCategory();
+
+        for(String s : head_ent_pair){
+            String[] pairList = s.split(";");
+            String head = pairList[1];
+            String answer = pairList[0];
+            try{
+                ArrayList<String> output = getCategory.catgoryScore(head,answer);
+                Thread.sleep(3000);
+                //System.out.println(score);
+                for(String t : output){
+                    bw.write(t);
+                    bw.write("\n");
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        try{
+
+            if(bw != null)
+                bw.close();
+            if(fw != null)
+                fw.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
 
 class Category{
